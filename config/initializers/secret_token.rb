792f1f9ca34cd8cd46eb64094e931ed3dfb5edaf
Diana Simakhov <dianad1218@yahoo.com>
2014-02-09 20:32:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobster::Application.config.secret_key_base = 'dd836dce17e599564c0a1c9327ec20cc2e203c9d0e14c6d60439d4f3f165a875863cbce108548748547c1dbe26aa49e0594b9888d1d917facb1737fecaf8ca1e'
